2a244af3227a93a14fab6d16ffd7d6b99bf9800d
qq283335746 <283335746@qq.com>
2019-08-08 14:37:59 +0800

    密码生成器

---
+++ src/WpfGenerate/GenerateHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Yibi.WpfGenerate
+{
+    public class GenerateHelper
+    {
+        private const string _sourceDefault = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string _sourceStrong = "`~!@#$%^&*()-_=+[]{}\\|;:'\",<.>/?";
+        private static List<char> _sourceDefaultDatas => _sourceDefault.ToCharArray().ToList();
+        private static int _sourceDefaultLength = _sourceDefault.Length;
+
+        public static string CreateGenerateCode(PasswordType passwordType)
+        {
+            List<char> datas = null;
+            int n = 0;
+
+            switch (passwordType)
+            {
+                case PasswordType.Default:
+                    datas = _sourceDefaultDatas;
+                    n = 20;
+                    break;
+                case PasswordType.Stronger:
+                    var strongSource = _sourceDefault + _sourceStrong;
+
+
+                    datas = strongSource.ToList<char>();
+                    n = 30;
+                    break;
+                default:
+                    throw new ArgumentException("不支持的密码类型", nameof(passwordType));
+            }
+
+            return string.Join("", CreateRandomCodes(datas,n));
+
+        }
+
+        private static IEnumerable<char> CreateRandomCodes(List<char> datas, int n)
+        {
+            byte[] bytes = new byte[n];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(bytes);
+            }
+
+            foreach (var item in bytes)
+            {
+                var index = item % datas.Count;
+
+                yield return datas[index];
+            }
+        }
+    }
+}
---
+++ src/WpfGenerate/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Yibi.WpfGenerate
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Bind();
+        }
+
+        private void Bind()
+        {
+            txtCode.Text = TxtCode;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txtCode.Text = TxtCode;
+        }
+
+        public string TxtCode
+        {
+            get
+            {
+                
+                var code = GenerateHelper.CreateGenerateCode(PasswordType);
+                Clipboard.SetText(code);
+                return code;
+            }
+        }
+
+        public PasswordType PasswordType { get; set; }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rbtn = sender as RadioButton;
+            switch (rbtn.Name)
+            {
+                case "rbtnDefaultType":
+                    PasswordType = PasswordType.Default;
+                    break;
+                case "rbtnStrongType":
+                    PasswordType = PasswordType.Stronger;
+                    break;
+                default:
+                    PasswordType = PasswordType.Default;
+                    break;
+            }
+
+            if(txtCode != null)
+            {
+                txtCode.Text = TxtCode;
+            }
+        }
+    }
+}
